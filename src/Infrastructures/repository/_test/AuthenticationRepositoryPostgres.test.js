f383ed5153ec06579e406e785b025b69704c358a
Zaenurrochman <zaenur.rochman98@gmail.com>
2021-09-05 12:14:35 +0700

    add refresh token process
    - add refresh authentication handler
    - add refresh authentication use case
    - add refresh authentication endpoint
    - Close #4

--- src/Infrastructures/repository/_test/AuthenticationRepositoryPostgres.test.js
+++ src/Infrastructures/repository/_test/AuthenticationRepositoryPostgres.test.js
@@ -32,5 +32,28 @@ describe('AuthenticationRepositoryPostgres', () => {
         expect(tokens[0].token).toBe(token);
       });
     });
+
+    describe('checkAvailabilityToken function', () => {
+      it('should throw InvariantError if token not available', async () => {
+        // Arrange
+        const authenticationRepository = new AuthenticationRepositoryPostgres(pool);
+        const token = 'token';
+
+        // Action & Assert
+        await expect(authenticationRepository.checkAvailabilityToken(token))
+          .rejects.toThrow(InvariantError);
+      });
+
+      it('should not throw InvariantError if token available', async () => {
+        // Arrange
+        const authenticationRepository = new AuthenticationRepositoryPostgres(pool);
+        const token = 'token';
+        await AuthenticationsTableTestHelper.addToken(token);
+
+        // Action & Assert
+        await expect(authenticationRepository.checkAvailabilityToken(token))
+          .resolves.not.toThrow(InvariantError);
+      });
+    });
   });
 });

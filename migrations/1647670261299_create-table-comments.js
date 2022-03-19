5aa9b02d1f78ac8c38ab5a02fbee7801bd27bb23
rochman25 <zaenur.rochman98@gmail.com>
2022-03-19 13:35:33 +0700

    add migration comment table

---
+++ migrations/1647670261299_create-table-comments.js
@@ -0,0 +1,37 @@
+/* eslint-disable camelcase */
+
+exports.shorthands = undefined;
+
+exports.up = (pgm) => {
+  pgm.createTable('comments', {
+    id: {
+      type: 'VARCHAR(50)',
+      primaryKey: true,
+    },
+    thread: {
+      type: 'VARCHAR(50)',
+      notNull: true,
+    },
+    content: {
+      type: 'TEXT',
+      notNull: true,
+    },
+    owner: {
+      type: 'VARCHAR(50)',
+      notNull: true,
+    },
+    created_at: {
+      type: 'TEXT',
+      notNull: false,
+    },
+    updated_at: {
+      type: 'TEXT',
+      notNull: false,
+    },
+  });
+
+  pgm.addConstraint('comments', 'fk_comments.thread_threads.id', 'FOREIGN KEY(thread) REFERENCES threads(id) ON DELETE CASCADE');
+  pgm.addConstraint('comments', 'fk_comments.owner_users.id', 'FOREIGN KEY(owner) REFERENCES users(id) ON DELETE CASCADE');
+};
+
+exports.down = (pgm) => {};

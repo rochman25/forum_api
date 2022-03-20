92d254222e61b1c317026dba7d7fe8fc82275b77
Zaenurrochman <zaenur.rochman98@gmail.com>
2022-03-20 22:04:53 +0700

    fix string test thread and add domain entities add comment

---
+++ src/Domains/comments/entities/_test/AddedComment.test.js
@@ -0,0 +1,39 @@
+const AddedComment = require('../AddedComment');
+
+describe('a AddedComment entities', () => {
+  it('should throw error when payload did not contain needed property', () => {
+    const payload = {
+      content: 'Ini Komentar',
+      owner: 'user-123',
+    };
+
+    expect(() => new AddedComment(payload)).toThrowError('ADDED_COMMENT.NOT_CONTAIN_NEEDED_PROPERTY');
+  });
+
+  it('should throw error when payload did not meet data type specification', () => {
+    const payload = {
+      id: 123,
+      content: 'Ini Komentar',
+      owner: true,
+    };
+
+    expect(() => new AddedComment(payload)).toThrowError('ADDED_COMMENT.NOT_MEET_DATA_TYPE_SPECIFICATION');
+  });
+
+  it('should create new added comment object correctly', () => {
+    // Arrange
+    const payload = {
+      id: 'comment-_pby2_tmXV6bcvcdev8xk',
+      content: 'Ini Komentar',
+      owner: 'user-123',
+    };
+
+    // Action
+    const addedComment = new AddedComment(payload);
+
+    // Assert
+    expect(addedComment.id).toEqual(payload.id);
+    expect(addedComment.content).toEqual(payload.content);
+    expect(addedComment.owner).toEqual(payload.owner);
+  });
+});
